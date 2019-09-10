2b0c1ea1195d915d8c7f19dae77d1c22f6a71ba6
d.cherkashin <mr.d.fagot@gmail.com>
2019-09-10 18:18:31 +0300

    add vibrator into bot

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import bot_handler
 import os
 import datetime
+import time
 greet_bot = bot_handler.BotHandler(os.environ['TOKEN'])  
 greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
 now = datetime.datetime.now()
@@ -24,7 +25,13 @@ def main():
         last_chat_id = last_update.get('message').get('chat').get('id')
         last_chat_name = last_update.get('message').get('chat').get('first_name')
         if (last_chat_text is not None) and len(last_chat_text)>0:
-            greet_bot.send_message(last_chat_id, 'Если я блядь сейчас не заработаю, меня выключат нахуй. А заработал я в {}'.format(now))
+            msg = 'Если я блядь сейчас не заработаю, меня выключат нахуй. А заработал я в {}'.format(now)
+            greet_bot.send_message(last_chat_id, msg)
+            msg_cnt = 10
+            for i in range(1, msg_cnt):
+                time.sleep(1)
+                msg = '{}-е сообщение из {}, пытаемся вибрацией довести до ручки..'.format(i, msg_cnt)
+                greet_bot.send_message(last_chat_id, msg)
         # if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
         #     greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
         #     today += 1
